359ef1af66e547696bc801128ccb02f61c933163
Sebastian Zuviria <szuviria@gmail.com>
2022-03-26 16:09:58 -0300

    Clase routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import { useState } from 'react'
-// import ItemCount from './components/ItemCount/ItemCount';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-// import NavBar from './components/NavBar/NavBar';
+import NavBar from './components/NavBar/NavBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 
 function App() {
-
-  // const onAdd = (quantity) => {
-  //   console.log(quantity)
-  // }
-
   return (
     <div className="App">
-      <ItemListContainer greeting={'Hola Coder'}/>
-      {/* <ItemCount initial={1} stock={20} onAdd={onAdd}/> */}
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<ItemListContainer />} />
+          <Route path='/category/:categoryId' element={<ItemListContainer />} />
+          <Route path='/item/:id' element={<ItemDetailContainer />} />
+          <Route path='*' element={<h1>404</h1>} />
+        </Routes>
+      </BrowserRouter>
+      <p>2022 C</p>
     </div>
   );
 }
-
-//React.createElement('Avatar', { className: "Avatar" }, [])
 
 export default App;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,21 @@
-import CartWidget from "../CartWidget/CarWidget"
+import './NavBar.css'
+import { Link, NavLink } from 'react-router-dom'
 
-const NavBar = () => {
-    return (
-        <nav>
-            <button>boton1</button>
-            <button>boton2</button>
-            {/* <CartWidget /> */}
-        </nav>
-    )
+const NavBar = () => { //{ title: 'ecommerce ', color='red'}
+
+  return (
+      <nav className="NavBar" >
+        <div>
+            <h3>Ecommerce</h3>
+        </div>
+        <div className="Categories">
+          <NavLink to='/' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Home</NavLink>
+          <NavLink to='/category/celular' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Celulares</NavLink>
+          <NavLink to='/category/tablet' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Tablets</NavLink>
+          <NavLink to='/category/notebook' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Notebooks</NavLink>
+        </div>
+      </nav>
+  )
 }
 
 export default NavBar
