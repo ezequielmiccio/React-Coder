7a7f7db9ff9bdb0191c067f39eb71577faa342b8
Sebastian Zuviria <szuviria@gmail.com>
2022-04-02 15:57:37 -0300

    Clase de context

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import './App.css';
+// import { useState, createContext } from 'react'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import Cart from './components/Cart/Cart';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { CartContextProvider } from './context/CartContext'
+
+// export const CartContext = createContext()
 
 function App() {
+  // const [cart, setCart] = useState([])
 
-  const handleKeyDown = (e) => {
-    if((e.key == 'a' && e.key == 'b') || e.key == 'e' || e.key == 'i' || e.key == 'o' || e.key == 'u') {
-      e.preventDefault()
-    }
-  }
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <NavBar />
-        <button onClick={(e) => console.log(e.nativeEvent)}>Boton</button>
-        <input onKeyDown={handleKeyDown}/>
-        <Routes>
-          <Route path='/' element={<ItemListContainer greeting={'Hola Coder'}/>}/>
-          <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Hola Coder'}/>} />
-          <Route path='/detail/:productId' element={<ItemDetailContainer />} />
-        </Routes>
-      </BrowserRouter>
+      {/* <CartContext.Provider value={'Este string esta en context'}> */}
+        <CartContextProvider>
+          <BrowserRouter>
+            <NavBar />
+            <Routes>
+              <Route path='/' element={<ItemListContainer greeting={'Hola Coder'}/>}/>
+              <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Hola Coder'}/>} />
+              <Route path='/detail/:productId' element={<ItemDetailContainer />} />
+              <Route path='/cart' element={<Cart />} />
+            </Routes>
+          </BrowserRouter>
+        </CartContextProvider>
+      {/* </CartContext.Provider> */}
     </div>
   );
 }
--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,13 +1,16 @@
 import './CartWidget.css'
+import { useContext } from 'react';
 import { Link } from 'react-router-dom'
-
+import CartContext from '../../context/CartContext'
 
 const CartWidget = () => {
+
+    const { getQuantity } = useContext(CartContext)
 
     return(
         <Link to={'/'} className="CartWidget">
             <img src="/images/cart.svg" alt='cart' className='CartImg'/>
-            5
+            { getQuantity() }
         </Link>
     );
 }
